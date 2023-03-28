249fee74dc3fd71ad756bf9b26f72e2298fffd02
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-03-28 21:58:36 +0200

    serialize is_breaking into output on CLI

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,8 @@
-use clap::Parser;
 use std::fs::File;
 use std::path::PathBuf;
 
+use clap::Parser;
+use serde::Serialize;
 use anyhow::Error;
 
 /// Compare old and new schema, and print differences
@@ -14,6 +15,14 @@ struct Args {
     rhs: PathBuf,
 }
 
+#[derive(Serialize)]
+struct Change {
+    #[serde(flatten)]
+    inner: json_schema_diff::Change,
+    is_breaking: bool
+
+}
+
 fn main() -> Result<(), Error> {
     let args = Args::parse();
 
@@ -23,6 +32,8 @@ fn main() -> Result<(), Error> {
     let changes = json_schema_diff::diff(lhs, rhs)?;
 
     for change in changes {
+        let is_breaking = change.change.is_breaking();
+        let change = Change { inner: change, is_breaking };
         println!("{}", serde_json::to_string(&change)?);
     }
     Ok(())
