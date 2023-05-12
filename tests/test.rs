5d27055c34a11046ea65796b3b7d249c6618da0f
Yudai Kiyofuji <57182816+6293@users.noreply.github.com>
2023-05-13 00:07:15 +0900

    include input values in snapshot for a smooth review (#28)

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,4 @@
-use insta::{assert_debug_snapshot, glob};
+use insta::{assert_debug_snapshot, glob, with_settings};
 use json_schema_diff::diff;
 use serde_json::Value;
 
@@ -6,9 +6,11 @@ use serde_json::Value;
 fn test_from_fixtures() {
     let test = |path: &std::path::Path| {
         let contents = std::fs::read_to_string(path).unwrap();
-        let value: Value = serde_json::from_str(&contents).unwrap();
-        let diff = diff(value["lhs"].clone(), value["rhs"].clone()).unwrap();
-        assert_debug_snapshot!(diff);
+        let values: Value = serde_json::from_str(&contents).unwrap();
+        let diff = diff(values["lhs"].clone(), values["rhs"].clone()).unwrap();
+        with_settings!({ info => &values }, {
+            assert_debug_snapshot!(diff);
+        });
     };
     glob!("../tests/fixtures", "**/*.json", test);
 }
